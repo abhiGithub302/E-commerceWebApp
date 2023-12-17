6e17e44958bfb88dbc62177997c8a88fc9b1bd72
abhishek <abhi19982014@gmail.com>
2023-12-17 15:26:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import  Counter  from './features/counter/Counter';
 // import Counter from './features/counter/Counter';
 
 import './App.css';
+import Home from './pages/Home';
 import ProductList from './features/product-list/ProductList';
-
 function App() {
   return (
     <div className="App">
+    <Home></Home>
     <ProductList></ProductList>
-      <Counter></Counter>
+      
     </div>
   );
 }
